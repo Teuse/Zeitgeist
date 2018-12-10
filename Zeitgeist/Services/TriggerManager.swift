1893ef2e172ab480bf7ae92f3e904bbee0a32899
Roder, Mathias <mathias.roder@daimler.com>
2018-12-10 10:09:50 +0100

    update

--- Zeitgeist/Services/TriggerManager.swift
+++ Zeitgeist/Services/TriggerManager.swift
@@ -2,49 +2,60 @@ import ReSwift
 
 class TriggerManager
 {
-    private let store: Store<AppState>
-    private var timer = Timer()
-    
-    private var startTimeTrigger = TimeTriggerProcessor() {
-        print("start timer trigger")
-    }
-    private var endTimeTrigger = TimeTriggerProcessor() {
-        print("start timer trigger")
-    }
-    
-    // --------------------------------------------------------------------------------
-    
-    init(store: Store<AppState>) {
-        self.store = store
-        setup()
-    }
-    
-    deinit {
-        store.unsubscribe(self)
-    }
+   private let store: Store<AppState>
+   private var timer = Timer()
 
-    private func setup()
-    {
-        store.subscribe(self) { subscriber in
-            subscriber.select { $0.triggerState }
-        }
-        
-        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block:
-            {[weak self] _ in self?.runLoop() })
-    }
-    
-    // --------------------------------------------------------------------------------
-    
-    private func runLoop()
-    {
-        
-    }
+   private lazy var startTimeTrigger: TimeTriggerProcessor = {
+      return TimeTriggerProcessor() { [weak self] in
+         self?.print()
+      }
+   }
+   private lazy var endTimeTrigger: TimeTriggerProcessor = {
+      return TimeTriggerProcessor() { [weak self] in
+         self?.print()
+      }
+   }
+
+   func print() {
+      print("start timer trigger")
+   }
+
+   // --------------------------------------------------------------------------------
+
+   init(store: Store<AppState>) {
+      self.store = store
+      setup()
+   }
+
+   deinit {
+      store.unsubscribe(self)
+   }
+
+   private func setup()
+   {
+      store.subscribe(self) { subscriber in
+         subscriber.select { $0.triggerState }
+      }
+
+      timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block:
+         {[weak self] _ in self?.runLoop() })
+   }
+
+   // --------------------------------------------------------------------------------
+
+   private func runLoop()
+   {
+
+   }
 }
 
 extension TriggerManager: StoreSubscriber
 {
-    func newState(state: TriggerState)
-    {
-        
-    }
+   func newState(triggerState: TriggerState)
+   {
+      if triggerState.startTimeTrigger.enabled {
+         startTimeTrigger.process()
+      }
+
+   }
 }
